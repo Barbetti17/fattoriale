08b4cf146157a3a45d717071482ec4991ad1d01d
Barbetti17 <francibieto2000@gmail.com>
2022-11-28 11:03:38 +0100

    Primi cambiamenti

---
+++ script.js
@@ -0,0 +1,17 @@
+function CalcolaFattoriale() {
+    let fattoriale;
+    let numero = document.getElementById("number").value;
+    const n = numero;
+
+    if(numero < 0) {
+        fattoriale = "il numero deve essere positivo!";
+    } else {
+        fattoriale = 1;
+        while(numero > 1) {
+            fattoriale *= numero;
+            numero -= 1;
+        }
+    }
+
+    document.getElementById("demo").innerHTML= n + "! = " + fattoriale;
+}
